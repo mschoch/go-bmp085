f8f58424fb13cf5efbbc91de20a1e4d51af1b2e7
Marty Schoch <marty.schoch@gmail.com>
2014-06-19 08:39:55 -0400

    new utility to read sensors and post to nowd

---
+++ post_to_nowd/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"log"
+	"math"
+	"net/http"
+
+	"github.com/mschoch/go-bmp085"
+)
+
+var url = flag.String("url", "http://localhost:4793/", "root url of nowd server")
+var device = flag.String("device", "sensor", "name of device")
+var elevation = flag.Float64("elevation", 0, "Elevation of sensor in meters")
+
+func main() {
+
+	flag.Parse()
+
+	var i2cbus byte = 1
+	d := new(bmp085.Device)
+	err := d.Init(i2cbus)
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	// set mode
+	d.SetMode(bmp085.ULTRAHIGHRES)
+
+	temp, err := d.ReadTemp()
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	pressure, err := d.ReadPressure()
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	adjustedPressure := float64(pressure) / math.Pow(1-(*elevation/44330), 5.255)
+	adjustedPressureHg := adjustedPressure / bmp085.TO_INCHES_MERCURY
+
+	jsonData := map[string]interface{}{
+		"t": temp,
+		"p": adjustedPressureHg,
+	}
+
+	jsonOut, err := json.Marshal(jsonData)
+	if err != nil {
+		log.Fatalf("error marshaling JSON: %v", err)
+	}
+	jsonBuffer := bytes.NewBuffer(jsonOut)
+	resp, err := http.Post(*url+"/"+*device, "application/json", jsonBuffer)
+	if err != nil {
+		log.Fatalf("error http post: %v", err)
+	}
+	log.Printf("resp: %s", resp.Status)
+}
